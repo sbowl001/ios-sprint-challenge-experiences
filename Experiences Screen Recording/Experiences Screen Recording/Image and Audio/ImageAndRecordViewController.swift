e246758c059a15aecd9c69dfa775653d7a5597f5
Stephanie <sbowl001@fiu.edu>
2019-09-30 23:31:11 +0800

    filter working

--- Experiences Screen Recording/Experiences Screen Recording/Image and Audio/ImageAndRecordViewController.swift
+++ Experiences Screen Recording/Experiences Screen Recording/Image and Audio/ImageAndRecordViewController.swift
@@ -26,20 +26,102 @@ class ImageAndRecordViewController: UIViewController {
     }
     
     @IBAction func addPosterImageTapped(_ sender: Any) {
+        self.presentImagePickerController()
     }
     @IBAction func recordButtonTapped(_ sender: Any) {
     }
     
     @IBAction func monochromeSliderAction(_ sender: Any) {
+        self.updateImage()
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+  
+    
+    //MARK: Private Func
+      private func presentImagePickerController() {
+            guard UIImagePickerController.isSourceTypeAvailable(.photoLibrary)
+                else {
+                    NSLog("The photo library is not available")
+                    return
+            }
+            
+            let imagePicker = UIImagePickerController()
+            imagePicker.sourceType = .photoLibrary
+            imagePicker.delegate = self
+            
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+        
+        private func image(byFiltering image: UIImage) -> UIImage {
+            
+            //UIImage > CGImage > CIImage
+            guard let cgImage = image.cgImage else {return image}
+            
+            let ciImage = CIImage(cgImage: cgImage)
+            
+            //set the values of the filter's paramenters
+            filter.setValue(ciImage, forKey: "inputImage")
+            filter.setValue(monochromeSlider.value, forKey: "inputIntensity")
+  
+            
+            //CIImage > CGImage > UIImage
+            guard let outputCIImage = filter.outputImage else {return image }
+            
+            guard let outputCGImage = context.createCGImage(outputCIImage, from: outputCIImage.extent) else { return image}
+            
+            return UIImage(cgImage: outputCGImage)
+        }
+        
+        private func updateImage() {
+            if let scaledImage = self.scaledImage {
+                imageView.image = self.image(byFiltering: scaledImage)
+            } else {
+                imageView.image = nil
+            }
+        }
+        
+        private func presentSuccessfulSaveAlert() {
+            let alert = UIAlertController(title: "PhotoSaved!", message: "The photo has been saved to your Photo library", preferredStyle: .alert)
+            
+            let okayAction = UIAlertAction(title: "Okay", style: .default, handler: nil)
+            alert.addAction(okayAction)
+            
+            present(alert, animated: true, completion: nil)
+        }
+ 
+    //MARK: Properties
+    
+    var originalImage: UIImage? {
+          didSet {
+              guard let originalImage = self.originalImage else {return}
+        
+              var scaledSize = imageView.bounds.size
+              
+              let scale = UIScreen.main.scale
+              scaledSize = CGSize(width: scaledSize.width * scale, height: scaledSize.height  * scale )
+              self.scaledImage = originalImage.imageByScaling(toSize: scaledSize)
+          }
+      }
+    
+    var scaledImage: UIImage? {
+        didSet {
+            updateImage()
+        }
     }
-    */
 
+    private let context = CIContext(options: nil)
+    private let filter = CIFilter(name: "CIColorMonochrome")!
+}
+
+extension ImageAndRecordViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[.originalImage] as? UIImage {
+            self.originalImage = image
+        }
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+         picker.dismiss(animated: true, completion: nil)
+    }
 }
